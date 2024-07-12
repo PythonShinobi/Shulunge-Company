9517340b71de6dae848488c86e9cd82e06b6f5ba
PythonShinobi <pythonshinobi@gmail.com>
2024-07-12 15:13:13 +0300

    Add route path for the about page.

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import "./App.css";
 import ContactForm from './contact/Contact';
+import AboutUs from './about/About';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+          <Route path='/about' element={<AboutUs />}/>
           <Route path='/contact' element={<ContactForm />}/>
       </Routes>
     </BrowserRouter>
