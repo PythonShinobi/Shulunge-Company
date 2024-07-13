507f4ea543ea2e378a1cc2655489943833127628
PythonShinobi <pythonshinobi@gmail.com>
2024-07-13 14:10:07 +0300

    Add route paths for BookKeeping, immigration, Payroll, SecreterialServices, Taxation and TaxServices components.

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,12 @@ import Home from './home/Home';
 import ContactForm from './contact/Contact';
 import AboutUs from './about/About';
 import ServicePage from './services-page/Services';
+import BookKeeping from './services-page/pages/BookKeeping';
+import Taxation from './services-page/pages/Taxation';
+import Payroll from './services-page/pages/Payroll';
+import TaxServices from './services-page/pages/TaxServices';
+import Secretarial from './services-page/pages/SecreterialServices';
+import Immigration from './services-page/pages/Immigration';
 
 const App = () => {
   return (
@@ -16,6 +22,12 @@ const App = () => {
           <Route path='/about' element={<AboutUs />}/>
           <Route path='/contact' element={<ContactForm />}/>
           <Route path='/services' element={<ServicePage />}/>
+          <Route path='/services/book-keeping' element={<BookKeeping />}/>
+          <Route path='/services/taxation' element={<Taxation />}/>
+          <Route path='/services/payroll' element={<Payroll />}/>
+          <Route path='/services/tax-services' element={<TaxServices />}/>
+          <Route path='/services/secretarial' element={<Secretarial />}/>
+          <Route path='/services/immigration' element={<Immigration />}/>
       </Routes>
     </BrowserRouter>
   );
